2209f0b3edc649b8360d78b712962d42e0343897
Charles Devane <devane.charles98@gmail.com>
2023-01-22 10:25:08 -0500

    install server dep too

--- client/cypress/e2e/auth_pages.cy.ts
+++ client/cypress/e2e/auth_pages.cy.ts
@@ -1,14 +1,14 @@
 describe("Auth flows", () => {
-  it("logs in user and redirects to dashboard", () => {
+  it.only("logs in user and redirects to dashboard", () => {
     cy.login();
     cy.url().should("include", "/account/dashboard");
     cy.get("h1").should("include.text", "signed in version");
   });
 
-  it.only("registers a new user", () => {
+  it("registers a new user", () => {
     cy.visit("/register");
     cy.get("#username").type("testergui");
-    cy.get("#email").type("test@gmail.com");
+    cy.get("#email").type("testing@gmail.com");
     cy.get("#password").type("test1234");
     cy.get("#password2").type("test1234");
     cy.get("#submit").click();
