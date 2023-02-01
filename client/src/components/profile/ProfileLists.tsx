d8cac74bc4234ccc2acf3d6b9a05220e0cafa19a
Charles DeVane <devane.charles98@gmail.com>
2023-02-01 16:34:13 -0500

    adding lists

--- client/src/components/profile/ProfileLists.tsx
+++ client/src/components/profile/ProfileLists.tsx
@@ -1,26 +1,13 @@
-import { useContext, useEffect, useState } from "react";
-import { ListContext } from "../../context/ListContext";
-import { api } from "../../utils/api";
+import { useLists } from '../../hooks/list'
 import ListsView from "../lists/ListsView";
 
 const ProfileLists = ({ id }: any) => {
-  const [lists, setLists] = useState([]);
-  const { getListById } = useContext(ListContext)
-
-  useEffect(() => {
-    getLists();
-    getListById('listid')
-  }, [id]);
-
-  const getLists = async () => {
-    const res = await api.get(`/lists/user/${id}`);
-    setLists(res.data);
-    console.log(res.data);
-  };
+  const { data, status } = useLists(id)
+  
   return (
     <div>
       <h1 className="text-3xl font-bold">Featured Lists</h1>
-      {lists && <ListsView lists={lists} />}
+      {data && <ListsView lists={data} />}
     </div>
   );
 };
---
+++ client/src/hooks/list.ts
@@ -0,0 +1,20 @@
+import { useQuery } from 'react-query'
+import { api } from '../utils/api'
+
+const getListByUserId = async (id : string) => {
+    const { data } = await api.get(`list/${id}`);
+    return data;
+  };
+  
+  export function useList() {
+    return useQuery("lists", async () => {
+      const { data } = await api.get("/lists");
+      return data;
+    });
+  }
+  
+  export function useLists(userId : string) {
+    return useQuery(["list", userId], () => getListByUserId(userId), {
+      enabled: !!userId,
+    });
+  }
